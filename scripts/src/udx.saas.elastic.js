40c223ae5498956bec2d63111f1cc880ee09bf71
Andy Potanin <andy.potanin@usabilitydynamics.com>
2014-02-07 02:41:21 -0500

    Automated commit.

--- scripts/src/udx.saas.elastic.js
+++ scripts/src/udx.saas.elastic.js
@@ -1,6 +1,10 @@
 /**
  * Client Elastic
  *
+ * @todo Add EventEmitter as a dependency to hand pub/sub.
+ * @todo Add udx.saas.socket as a dependency and use as a client for Elastic.
+ * @todo Perhaps add udx.storage to cache certain data sets, or use saving preferences/queries/whatever.
+ *
  * @version 0.1.0
  * @returns {Object}
  */
@@ -8,26 +12,53 @@ define( 'udx.saas.elastic', [ 'udx.utility', 'elastic.client' ], function() {
   console.debug( 'udx.saas.elastic', 'loaded' );
 
   var _local = window.ejs;
-
   window.ejs = null;
 
-  //console.log( '_local', _local );
-
   function Elastic( options ) {
-    console.debug( 'udx.saas.elastic', 'created' );
+    console.debug( 'udx.saas.elastic', 'Elastic' );
 
-    _local.client = {
-      get: function() {
-        console.log( 'get', arguments );
+    // Event Store.
+    this._events = {};
+
+    return Object.create( _local, {
+      emit: {
+        value: function emit() {
+          console.debug( 'Elastic', 'emit' );
+
+        },
+        enumerable: true,
+        configurable: true,
+        writable: true
       },
-      set: function() {
-        console.log( 'set', arguments );
+      on: {
+        value: function on() {
+          console.debug( 'Elastic', 'on' );
+
+        },
+        enumerable: true,
+        configurable: true,
+        writable: true
+      },
+      _events: {
+        value: this._events,
+        enumerable: true,
+        configurable: true,
+        writable: true
+      },
+      client: {
+        value: {
+          get: function() {
+            console.log( 'get', arguments );
+          },
+          set: function() {
+            console.log( 'set', arguments );
+          }
+        },
+        enumerable: true,
+        configurable: true,
+        writable: true
       }
-    };
-
-    return _local;
-
-    //return this;
+    });
 
   }
 
@@ -36,9 +67,9 @@ define( 'udx.saas.elastic', [ 'udx.utility', 'elastic.client' ], function() {
    *
    */
   Object.defineProperties( Elastic.prototype, {
-    setItem: {
-      value: function setItem( key, value ) {
-        return this.get( key, value );
+    set: {
+      value: function set( key, value ) {
+        //return this.get( key, value );
       },
       enumerable: true,
       configurable: true,
@@ -54,19 +85,17 @@ define( 'udx.saas.elastic', [ 'udx.utility', 'elastic.client' ], function() {
     create: {
       /**
        *
-       * @param name {String|Null}
        * @param options {Object|Null}
-       * @returns {settings.Elastic}
        */
-      value: function create( name, options ) {
-        return new Elastic( name, options )
+      value: function create( options ) {
+        return new Elastic( options )
       },
       enumerable: true,
       configurable: true,
       writable: true
     },
     version: {
-      value: '1.0.1',
+      value: '1.0.0',
       enumerable: false,
       writable: false
     }
